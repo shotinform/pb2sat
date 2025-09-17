697453d256980a0a77fa6bf6409d616eab710b3e
Marko <marko.nikitovic01@gmail.com>
2025-09-17 23:01:20 +0200

    after change

--- main.cpp
+++ main.cpp
@@ -173,13 +173,13 @@ void seqcounter_geq(const std::vector<int>& X, int k, CNF& cnf) {
         cnf.addClause({ -X[i-1],  s[i][1] });                     
         for (int j = 1; j <= J; ++j) cnf.addClause({ -s[i-1][j],  s[i][j] });
         for (int j = 1; j <= J-1; ++j) cnf.addClause({ -X[i-1], -s[i-1][j],  s[i][j+1] }); 
-        for (int j = 1; j <= J-1; ++j) cnf.addClause({  X[i-1],  s[i-1][j+1], -s[i][j+1] }); 
+        for (int j = 0; j <= J-1; ++j) cnf.addClause({  X[i-1],  s[i-1][j+1], -s[i][j+1] }); 
         for (int j = 1; j <= J-1; ++j) cnf.addClause({  s[i-1][j], s[i-1][j+1], -s[i][j+1] });
     }
 
                     
-    for (int i = 2; i <= n; ++i)
-        cnf.addClause({ -s[i][1], s[i-1][1], X[i-1] });
+    //for (int i = 2; i <= n; ++i)
+    //    cnf.addClause({ -s[i][1], s[i-1][1], X[i-1] });
 
 
     cnf.addClause({ s[n][k] }); 
